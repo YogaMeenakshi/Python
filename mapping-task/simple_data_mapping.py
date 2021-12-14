e20b6aa99823547957307b68a61ecdefccd4344d
ESPL6933 <Sasikumar.Ramakrishnan@episourcein.episource.com>
2021-12-14 09:25:07 +0530

    Update simple_data_mapping.py

--- mapping-task/simple_data_mapping.py
+++ mapping-task/simple_data_mapping.py
@@ -4,9 +4,10 @@
 
 import models
 import requests
-import json
 from datetime import datetime
 import re
+import schedule
+import time
 
 url_list = {
     "list": "https://mapping-test.fra1.digitaloceanspaces.com/data/list.json",
@@ -34,47 +35,54 @@ def remove_html_tags(text_with_html):
     return text
 
 
-articles_list = connect(url_list["list"])
-# print(articles_list)
+def process_article(article_list):
+    for article in article_list:
+        article_id = article["id"]
+        details = connect(url_list["detail"].format(article_id))
+        media = connect(url_list["media"].format(article_id))
 
-for article in articles_list:
-    article_id = article["id"]
-    # print(url_list["media"].format(article_id))
-    details = connect(url_list["detail"].format(article_id))
-    media = connect(url_list["media"].format(article_id))
-    # print(details)
-    # print(media)
-    if details:
-        sections = details["sections"]
-        # del_items = []
-        for _sections in sections:
-            # print(_sections)
-            if _sections.get("text"):
-                _sections["text"] = remove_html_tags(_sections["text"])
-            _type = _sections["type"]
+        if details:
+            sections = details["sections"]
+            for _sections in sections:
+                if _sections.get("text"):
+                    _sections["text"] = remove_html_tags(_sections["text"])
+                _type = _sections["type"]
 
-            if _type == "media":
-                _id = _sections["id"]
-                for m in media:
-                    if m["id"] == _id:
-                        sections.remove(_sections)
+                if _type == "media":
+                    _id = _sections["id"]
+                    for m in media:
+                        if m["id"] == _id:
+                            sections.remove(_sections)
 
-                        if m.get("pub_date"):
-                            m["publication_date"] = datetime.strptime(m["pub_date"].replace(";", ":"),
-                                                                  '%Y-%m-%d-%H:%M:%S')
-                        if m.get("mod_date"):
-                            m["modification_date"] = datetime.strptime(m["mod_date"].replace(";", ":"),
-                                                                       '%Y-%m-%d-%H:%M:%S')
-                        sections.append(m)
+                            if m.get("pub_date"):
+                                m["publication_date"] = datetime.strptime(m["pub_date"].replace(";", ":"),
+                                                                          '%Y-%m-%d-%H:%M:%S')
+                            if m.get("mod_date"):
+                                m["modification_date"] = datetime.strptime(m["mod_date"].replace(";", ":"),
+                                                                           '%Y-%m-%d-%H:%M:%S')
+                            sections.append(m)
 
-        details["publication_date"] = datetime.strptime(details["pub_date"].replace(";", ":"), '%Y-%m-%d-%H:%M:%S')
-        if details.get("mod_date"):
-            details["modification_date"] = datetime.strptime(details["mod_date"].replace(";", ":"), '%Y-%m-%d-%H:%M:%S')
+            details["publication_date"] = datetime.strptime(details["pub_date"].replace(";", ":"), '%Y-%m-%d-%H:%M:%S')
+            if details.get("mod_date"):
+                details["modification_date"] = datetime.strptime(details["mod_date"].replace(";", ":"),
+                                                                 '%Y-%m-%d-%H:%M:%S')
 
+            try:
+                test = models.Article(**details)
+                print(test.dict())
+            except Exception as e:
+                print(details)
+                print(e)
+
+
+if __name__ == '__main__':
+    articles_list = connect(url_list["list"])
+    schedule.every(1).minutes.do(process_article, articles_list)
+
+    while True:
         try:
-            test = models.Article(**details)
-            print(test.dict())
+            schedule.run_pending()
+            time.sleep(1)
         except Exception as e:
-            print(details)
-            print(e)
-    # print(test.dict())
+            print("Not able to process the articles!")
+            raise Exception(e)
